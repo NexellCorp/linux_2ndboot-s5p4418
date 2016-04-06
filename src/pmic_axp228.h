560b10e04215b5725de2d8b2bc5196dbdc1f74d8
DeokJin Lee <truevirtue@nexell.co.kr>
2016-04-06 14:48:46 +0900

    s5p4418: axp228 pmic voltage change.
    [modify]
    - axp228 pmic voltage change.
    
    Signed-off-by: DeokJin Lee <truevirtue@nexell.co.kr>

--- src/pmic_axp228.h
+++ src/pmic_axp228.h
@@ -23,7 +23,7 @@
 #define AXP228_DEF_DDC1_VOL_MIN         1600000 /* UINT = 1uV, 1.6V */
 #define AXP228_DEF_DDC1_VOL_MAX         3400000 /* UINT = 1uV, 3.4V */
 
-#define AXP228_DEF_DDC234_VOL_MIN       660000  /* UINT = 1uV, 0.66V */
+#define AXP228_DEF_DDC234_VOL_MIN       600000  /* UINT = 1uV, 0.60V */
 #define AXP228_DEF_DDC24_VOL_MAX        1540000 /* UINT = 1uV, 1.54V */
 #define AXP228_DEF_DDC3_VOL_MAX         1860000 /* UINT = 1uV, 1.86V */
 #define AXP228_DEF_DDC5_VOL_MIN         1050000 /* UINT = 1uV, 1.05V */
@@ -33,7 +33,8 @@
 #define AXP228_DEF_DDC234_VOL_STEP      20000   /* UINT = 1uV, 20.0mV */
 #define AXP228_DEF_DDC5_VOL_STEP        50000   /* UINT = 1uV, 50.0mV */
 
-#define AXP228_DEF_DDC2_VOL             1125000 /* VAL(uV) = 0: 0.60 ~ 1.54V, Step 20 mV, default(OTP) = 1.1V */
+#define AXP228_DEF_DDC2_VOL             1250000 /* VAL(uV) = 0: 0.60 ~ 1.54V, Step 20 mV, default(OTP) = 1.25V */
+#define AXP228_DEF_DDC3_VOL             1100000 /* VAL(uV) = 0: 0.60 ~ 1.86V, Step 20 mV, default(OTP) = 1.1V */
 #define AXP228_DEF_DDC4_VOL             1500000 /* VAL(uV) = 0: 0.60 ~ 1.54V, Step 20 mV, default(OTP) = 1.5V */
 #define AXP228_DEF_DDC5_VOL             1500000 /* VAL(uV) = 0: 0.60 ~ 1.54V, Step 50 mV, default(OTP) = 1.5V */
 
